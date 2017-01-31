5e94b7c2bdebec1540f14e1190d5eeface1ec7dc
marek <marek.laskowski@senecacollege.ca>
2017-01-31 14:30:48 -0500

    fixed layout problems

--- app/src/main/java/com/example/mareklaskowski/lab3starter/MainActivity.java
+++ app/src/main/java/com/example/mareklaskowski/lab3starter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mareklaskowski.lab3starter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("hello world");
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
+        //LayoutParams is a class to specify layout properties when adding a view to a layout
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(textView, layoutParams);
+
+        //TODO: change things so that TextViews are added in response to button clicks
+        //TODO: populate the AutoCompleteTextView using an ArrayAdapter for String arrays
     }
 }
